86afcaa4a590ebe6e2b2847fbd647f7fb50d038e
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-23 21:20:36 +0530

    content script change

--- js/twitter-night-mode.js
+++ js/twitter-night-mode.js
@@ -1,5 +1,13 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('twitter-night-mode.css');
-(document.head||document.documentElement).appendChild(style);
+function setNightMode() {
+	var style = document.createElement('link');
+	style.rel = 'stylesheet';
+	style.type = 'text/css';
+	style.href = chrome.extension.getURL('twitter-night-mode.css');
+	(document.head||document.documentElement).appendChild(style);
+}
+
+if(document.URL == 'https://twitter.com/'
+		|| document.URL == 'https://twitter.com'
+		|| document.URL.indexOf('https://twitter.com/i/cards/') === 0) {
+	setNightMode();
+}
