0bcef1b62b7dfedcb7bae12e84521615d22e3c40
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-24 15:48:05 +0530

    Firefox compatibility

--- js/twitter-night-mode-bg.js
+++ js/twitter-night-mode-bg.js
@@ -1,10 +1,11 @@
+var c = chrome || browser;
+
 function setNightMode(tab) {
-	chrome.tabs.insertCSS(tab.id, { file: 'twitter-night-mode.css', allFrames: true});
+	c.tabs.insertCSS(tab.id, { file: 'twitter-night-mode.css', allFrames: true});
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+c.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(tab.url && tab.url === 'https://twitter.com/') {
 		setNightMode(tab);
 	}
 });
-
