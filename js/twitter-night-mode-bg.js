587dce7790324416c6a3b11606bc99eea418b6a9
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-25 19:12:55 +0530

    controlling theme using page action :tada:

--- js/twitter-night-mode-bg.js
+++ js/twitter-night-mode-bg.js
@@ -1,11 +1,42 @@
 var c = chrome || browser;
 
+c.pageAction.onClicked.addListener(function(tab) {
+  c.storage.local.get({'nightmode': true}, function(items) {
+    if(items.nightmode) {
+      unsetNightMode(tab);
+      c.tabs.reload(tab.id);
+    }else {
+      setNightMode(tab);
+    }
+  });
+});
+
+function unsetNightMode(tab) {
+  c.storage.local.set({'nightmode': false}, function() {
+    c.pageAction.setIcon({tabId: tab.id, path: 'images/icon32-blue.png'});
+    c.pageAction.setTitle({tabId: tab.id, title: 'Night mode disabled. Click to enable.'});
+  });
+}
+
 function setNightMode(tab) {
-	c.tabs.insertCSS(tab.id, { file: 'css/main.css', allFrames: true});
+  c.storage.local.set({'nightmode': true}, function() {
+    c.tabs.insertCSS(tab.id, { file: 'css/main.css', allFrames: true});
+    c.pageAction.setIcon({tabId: tab.id, path: 'images/icon32.png'});
+    c.pageAction.setTitle({tabId: tab.id, title: 'Night mode enabled. Click to disable.'});
+  });
 }
 
 c.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(tab.url && tab.url === 'https://twitter.com/') {
-		setNightMode(tab);
-	}
+    c.pageAction.show(tab.id);
+    c.storage.local.get({'nightmode': true}, function(items) {
+      if(items.nightmode) {
+        setNightMode(tab);
+      }else {
+        unsetNightMode(tab);
+      }
+    });
+	}else {
+    c.pageAction.setTitle({tabId: tab.id, title: 'Extension will be enabled if you open twitter.com'});
+  }
 });
--- js/twitter-night-mode.js
+++ js/twitter-night-mode.js
@@ -1,13 +1,19 @@
+chrome.storage.local.get({'nightmode': true}, function(items) {
+  if(items.nightmode) {
+    setNightMode();
+  }
+});
+
 function setNightMode() {
-	var style = document.createElement('link');
-	style.rel = 'stylesheet';
-	style.type = 'text/css';
-	style.href = chrome.extension.getURL('css/main.css');
-	(document.head||document.documentElement).appendChild(style);
+  if(document.URL == 'https://twitter.com/'
+    || document.URL == 'https://twitter.com'
+    || document.URL.indexOf('https://twitter.com/i/cards/') === 0) {
+
+    var style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = chrome.extension.getURL('css/main.css');
+    (document.head||document.documentElement).appendChild(style);
+  }
 }
 
-if(document.URL == 'https://twitter.com/'
-		|| document.URL == 'https://twitter.com'
-		|| document.URL.indexOf('https://twitter.com/i/cards/') === 0) {
-	setNightMode();
-}
