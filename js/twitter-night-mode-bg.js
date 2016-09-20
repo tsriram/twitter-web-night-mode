e367d24c727a8f47ca615c358490887b1cca4ace
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-09-20 16:56:03 +0530

    added google analytics to track usage of extension

--- js/twitter-night-mode-bg.js
+++ js/twitter-night-mode-bg.js
@@ -1,6 +1,11 @@
 var c = chrome || browser;
 var regex = /https:\/\/twitter\.com\/(.*)/;
 
+// track usage via Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-82144226-2']);
+_gaq.push(['_trackPageview']);
+
 c.pageAction.onClicked.addListener(function(tab) {
   c.storage.local.get({'nightmode': true}, function(items) {
     if(items.nightmode) {
@@ -17,6 +22,7 @@ function unsetNightMode(tab) {
     c.pageAction.setIcon({tabId: tab.id, path: 'images/icon32-blue.png'});
     c.pageAction.setTitle({tabId: tab.id, title: 'Night mode disabled. Click to enable.'});
   });
+  _gaq.push(['_trackEvent', 'nightmode-action', 'disable']);
 }
 
 function setNightMode(tab) {
@@ -25,6 +31,7 @@ function setNightMode(tab) {
     c.pageAction.setIcon({tabId: tab.id, path: 'images/icon32.png'});
     c.pageAction.setTitle({tabId: tab.id, title: 'Night mode enabled. Click to disable.'});
   });
+  _gaq.push(['_trackEvent', 'nightmode-action', 'enable']);
 }
 
 c.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -41,3 +48,13 @@ c.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     c.pageAction.setTitle({tabId: tab.id, title: 'Extension will be enabled if you open twitter.com'});
   }
 });
+
+// Google Analytics
+c.storage.local.get({'nightmode': true}, function(items) {
+  _gaq.push(['_trackEvent', 'nightmode', items.nightmode ? 'enabled' : 'disabled']);
+});
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
