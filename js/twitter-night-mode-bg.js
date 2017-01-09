6f5fb55f7b4a8ace4061c7d88cdf6203e4d846d2
Sriram Thiagarajan <sri.sjc@gmail.com>
2017-01-10 00:04:00 +0530

    survey!

--- js/twitter-night-mode-bg.js
+++ js/twitter-night-mode-bg.js
@@ -1,6 +1,9 @@
 var c = chrome || browser;
 var regex = /https:\/\/twitter\.com\/(.*)/;
 
+// a small survey to know why people uninstall
+c.runtime.setUninstallURL('https://goo.gl/forms/tEOXKxPjYpHsgoOj1');
+
 c.pageAction.onClicked.addListener(function(tab) {
   c.storage.local.get({'nightmode': true}, function(items) {
     if(items.nightmode) {
